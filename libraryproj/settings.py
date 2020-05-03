1c7571653757e1d3c3e9f7177183937f87752de9
Agnieszka Rudnicka <agnieszka.rudnicka@iisg.agh.edu.pl>
2020-05-03 18:25:37 +0200

    Allowed hosts

--- libraryproj/settings.py
+++ libraryproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i^5ozeoskp-yc$5s7)=%t)(5$h1i%!_udbg60dxwod3m^rdoj_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
